fabfbf73b2bdcc803072757e6d4bb15564c47c1d
Katherin Uy <katherinenoelleuy.iics@ust.edu.ph>
2021-11-24 13:45:14 +0800

    moreno info added

---
+++ moreno.php
@@ -0,0 +1,74 @@
+<?php require('components/header.inc.php'); ?>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-5">
+                <a class="navbar-brand" href="index.php">Presidential Candidates</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Achievements</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Authored Bills Passed</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Legislative Works</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="./assets/img/isko.png" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">Francisco Moreno Domagoso</h1>
+                    <p>Francisco Moreno Domagoso, also known by his stage name Isko Moreno, is a Filipino politician and actor currently 
+                        serving as the 22nd mayor of Manila since 2019. Before heading the capital of the Philippines, he started to gain notability as an actor and television personality.</p>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-black m-0"> “I will be a healing president. While ours will be a government of national reconstruction, 
+                    it will also be a government of national reconciliation, based on justice and rule of law.”</p></div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">About</h2>
+                            <p class="card-text">Politically, Moreno is identified as a populist. Moreno himself described his leadership style as akin to that of his predecessor Alfredo Lim, citing their shared commitment in reducing crime and maintaining cleanliness of the capital.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Awards and Achievements</h2>
+                            <p class="card-text">Man of the Year of the Asia Leaders Award</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Authored Bills Passed</h2>
+                            <p class="card-text"></p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Cult of Kath 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
