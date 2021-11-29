93e0d290b6256d3c3d1b88cb53551810988d38a1
Katherin Uy <katherinenoelleuy.iics@ust.edu.ph>
2021-11-29 23:02:03 +0800

    added admin header

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('components/admin-header.inc.php'); ?>
 <?php 
     include "server.php";
 ?>
@@ -11,6 +12,14 @@
     <body style="background-color:#ECF0F1;">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Admin</title>
+
+        <!-- Header-->
+        <header class="admin-header">
+            <div class="container">
+                <h1 class="head-1">Title</h1>
+            </div>
+        </header>
+
 </head>
 <body>
 
