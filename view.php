6d8f7f11f3543ddd4c54cbe6c5e165c051f7d775
Katherin Uy <katherinenoelleuy.iics@ust.edu.ph>
2021-11-29 13:07:52 +0800

    how to vote added

--- view.php
+++ view.php
@@ -52,33 +52,34 @@
         <section class="content-section bg-primary text-white text-center" id="services">
             <div class="container px-4 px-lg-5">
                 <div class="content-section-heading">
-                    <h3 class="text-secondary mb-0">Services</h3>
+                    <h3 class="text-secondary mb-0">Guide</h3>
                     <h2 class="mb-5">How to Vote for 2022</h2>
                 </div>
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
                         <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-screen-smartphone"></i></span>
-                        <h4><strong>Responsive</strong></h4>
-                        <p class="text-faded mb-0">Looks great on any screen size!</p>
+                        <h4><strong>Identify yourself</strong></h4>
+                        <p class="text-faded mb-0">Look for your name on the voters’ list posted by your polling precinct.</p>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
                         <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-pencil"></i></span>
-                        <h4><strong>Redesigned</strong></h4>
-                        <p class="text-faded mb-0">Freshly redesigned for Bootstrap 5.</p>
+                        <h4><strong>Get you ballot</strong></h4>
+                        <p class="text-faded mb-0">approach the board of elections inspectors (BEI) chairperson to get your ballot, 
+                            ballot secrecy folder, and marking pen.</p>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
                         <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-like"></i></span>
-                        <h4><strong>Favorited</strong></h4>
+                        <h4><strong>Choose candidates</strong></h4>
                         <p class="text-faded mb-0">
-                            Millions of users
-                            <i class="fas fa-heart"></i>
-                            Start Bootstrap!
+                        Take note, you cannot vote for more 
+                        candidates than what it listed beside each position as this can invalidate your vote. 
                         </p>
                     </div>
                     <div class="col-lg-3 col-md-6">
-                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-mustache"></i></span>
-                        <h4><strong>Question</strong></h4>
-                        <p class="text-faded mb-0">I mustache you a question...</p>
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-envelope"></i></span>
+                        <h4><strong>Cast your ballot</strong></h4>
+                        <p class="text-faded mb-0">After filling out your ballot, keep it inside the ballot secrecy folder. 
+                            Bring this to the VCM and insert it into the machine.</p>
                     </div>
                 </div>
             </div>
