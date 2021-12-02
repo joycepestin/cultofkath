5b3a937653f9a72c88bd7333c3bcf686d3ed3905
denverallam <denver.allam.iics@ust.edu.ph>
2021-12-02 23:42:49 +0800

    with composer

--- login.php
+++ login.php
@@ -45,7 +45,7 @@
 </head>
 <body>
     
-<div class="login-wrap">
+<div class="login-wrap my-4">
 	<div class="login-html">
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
 		<input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab">Forgot Password</label>
