6a9cc1173b65be60f98c77f2b091e56f5ee61ec9
denverallam <denver.allam.iics@ust.edu.ph>
2021-12-02 20:52:14 +0800

    with validation

--- forms/candidate_form.php
+++ forms/candidate_form.php
@@ -26,7 +26,7 @@
             header("location: achievement_form.php?candidate_id=$id&candidate_name=$candidate_name&editing=1");
         }
         else{
-            if(isset($_REQUEST["full_name"]) && isset($_REQUEST["description"]) && $_FILES['image']){
+            if(isset($_REQUEST["full_name"]) && isset($_REQUEST["description"]) && $_FILES['image'] && $_REQUEST["full_name"] != "" && $_REQUEST["description"] != "" &&  $_FILES["image"] != "" ){
 
                 $img_name = $_FILES['image']['name'];
                 $img_size = $_FILES['image']['size'];
@@ -151,7 +151,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Title</title>
+    <title>Candidate Form</title>
 </head>
 <body>
 
@@ -222,10 +222,12 @@
     </div>
     <?php } ?>
 
-            <div class="container mx-auto">
+    <div class="container mx-auto">
+            <div class="d-flex justify-content-center align-items-center">
                 <form method="POST" action="../logout.php">
-                            <button name="edit" class="btn btn-danger">Logout</button>
+                                <button name="edit" class="btn btn-danger">Logout</button>
                 </form>
             </div>
+        </div>
 </body>
 </html>
--- forms/legislative_form.php
+++ forms/legislative_form.php
@@ -8,7 +8,7 @@
     $id = $_REQUEST["candidate_id"];
 
     if(isset($_REQUEST["add_legislative"])){
-        if(isset($_REQUEST["candidate_id"]) && isset($_REQUEST["description"])){
+        if(isset($_REQUEST["candidate_id"]) && isset($_REQUEST["description"]) &&  $_REQUEST["description"] != ""){
             $description = $_REQUEST["description"];
             $legislative->createLegislativeWork($conn, $id, $description);
         }
@@ -54,7 +54,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Title</title>
+    <title>Legislative Work Form</title>
 </head>
 <body>
     <div class="container my-5" style="max-width:60%, min-width:50%">
